7bbae21661f0c16659fbeeddfb91df00aa9f4c66
doudoudzj <doudoudzj@sina.com>
2018-10-24 03:23:52 +0800

    update

--- static/js/directives.js
+++ static/js/directives.js
@@ -7,39 +7,43 @@ directive('navbar', function(){
 		controller: ['$scope', '$rootScope', function($scope, $rootScope){
 			$rootScope.navbar_loaded = true;
 		}],
-		template: '<nav class="navbar navbar-default">\
-				<div class="container-fluid">\
-					<div class="navbar-header">\
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">\
-                <span class="sr-only">Toggle navigation</span>\
-                <span class="icon-bar"></span>\
-                <span class="icon-bar"></span>\
-                <span class="icon-bar"></span>\
-              </button>\
-              <a class="navbar-brand" href="#/main">Intranet</a>\
-          </div>\
-          <div id="navbar" class="navbar-collapse collapse">\
-            <ul class="nav navbar-nav">\
-              <li ng-class="\'active\' | ifmatch:[currentItem,\'main\']"><a href="#/main">首页</a></li>\
-              <li ng-class="\'active\' | ifmatch:[currentItem,\'service(\..*)?\']"><a href="#/service">服务管理</a></li>\
-              <li ng-class="\'active\' | ifmatch:[currentItem,\'file\']"><a href="#/file">文件管理</a></li>\
-              <li ng-class="\'active\' | ifmatch:[currentItem,\'site(\..*)?\']"><a href="#/site">网站管理</a></li>\
-              <li ng-class="\'active\' | ifmatch:[currentItem,\'database\']"><a href="#/database">数据库管理</a></li>\
-              <li ng-class="\'active\' | ifmatch:[currentItem,\'ftp\']"><a href="#/ftp">FTP管理</a></li>\
-              <li ng-class="\'active\' | ifmatch:[currentItem,\'secure\']"><a href="#/secure">安全管理</a></li>\
-              <li ng-class="\'active\' | ifmatch:[currentItem,\'backup\']"><a href="#/backup">备份管理</a></li>\
-              <li ng-class="\'active\' | ifmatch:[currentItem,\'log\']"><a href="#/log">日志管理</a></li>-->\
-              <li ng-class="\'active\' | ifmatch:[currentItem,\'task\']"><a href="#/task">计划任务</a></li>\
-              <li ng-class="\'active\' | ifmatch:[currentItem,\'utils(\..*)?\']"><a href="#/utils">系统工具</a></li>\
-            </ul>\
-            <ul class="nav navbar-nav navbar-right">\
-              <li ng-class="\'active\' | ifmatch:[currentItem,\'setting(\..*)?\']"><a href="#/setting">设置</a></li>\
-              <li class="divider-vertical"></li>\
-              <li ng-class="\'active\' | ifmatch:[currentItem,\'logout\']"><a href="#/logout">退出</a></li>\
-            </ul>\
-          </div>\
-				</div>\
-			</nav>',
+		template: '<nav class="navbar navbar-default"><div class="container-fluid">\
+            <div class="navbar-header">\
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">\
+                    <span class="sr-only">Toggle navigation</span>\
+                    <span class="icon-bar"></span>\
+                    <span class="icon-bar"></span>\
+                    <span class="icon-bar"></span>\
+                </button>\
+                <a class="navbar-brand" href="#/main">Intranet</a>\
+            </div>\
+            <div id="navbar" class="navbar-collapse collapse">\
+                <ul class="nav navbar-nav">\
+                    <li ng-class="\'active\' | ifmatch:[currentItem,\'main\']"><a href="#/main">首页</a></li>\
+                    <li ng-class="\'active\' | ifmatch:[currentItem,\'service(\..*)?\']"><a href="#/service">服务管理</a></li>\
+                    <li ng-class="\'active\' | ifmatch:[currentItem,\'file\']"><a href="#/file">文件管理</a></li>\
+                    <li ng-class="\'active\' | ifmatch:[currentItem,\'site(\..*)?\']"><a href="#/site">网站管理</a></li>\
+                    <li ng-class="\'active\' | ifmatch:[currentItem,\'database\']"><a href="#/database">数据库管理</a></li>\
+                    <li class="dropdown">\
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">系统工具 <span class="caret"></span></a>\
+                        <ul class="dropdown-menu">\
+                            <li ng-class="\'active\' | ifmatch:[currentItem,\'ftp\']"><a href="#/ftp">FTP管理</a></li>\
+                            <li ng-class="\'active\' | ifmatch:[currentItem,\'task\']"><a href="#/task">计划任务</a></li>\
+                            <li role="separator" class="divider"></li>\
+                            <li ng-class="\'active\' | ifmatch:[currentItem,\'utils(\..*)?\']"><a href="#/utils">系统工具</a></li>\
+                            <li ng-class="\'active\' | ifmatch:[currentItem,\'secure\']"><a href="#/secure">安全管理</a></li>\
+                            <li ng-class="\'active\' | ifmatch:[currentItem,\'backup\']"><a href="#/backup">备份管理</a></li>\
+                            <li ng-class="\'active\' | ifmatch:[currentItem,\'log\']"><a href="#/log">日志管理</a></li>\
+                        </ul>\
+                    </li>\
+                </ul>\
+                <ul class="nav navbar-nav navbar-right">\
+                    <li ng-class="\'active\' | ifmatch:[currentItem,\'setting(\..*)?\']"><a href="#/setting">设置</a></li>\
+                    <li class="divider-vertical"></li>\
+                    <li ng-class="\'active\' | ifmatch:[currentItem,\'logout\']"><a href="#/logout">退出</a></li>\
+                </ul>\
+            </div>\
+        </div></nav>',
 		replace: true
 	};
 }).
