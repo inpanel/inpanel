88cd29b98eec3e75cdd1e31e9d22285f7640c08b
唐钰豆豆 <doudoudzj@gmail.com>
2019-01-21 19:56:50 +0800

    ACME test

--- test/a.py
+++ test/a.py
@@ -1,36 +1,7 @@
 # -*- coding: utf-8 -*-
 # 获取进程列表
 
-# import psutil
-import re
-# for proc in psutil.process_iter():
-#     try:
-#         pinfo = proc.as_dict(attrs=['pid', 'name'])
-#     except psutil.NoSuchProcess:
-#         pass
-#     else:
-#         print(pinfo)
+import sys
+aa = sys.version_info
 
-f = open('/Users/douzhenjiang/Projects/intranet-panel/test/a.py')
-print f.readline()
-
-a = '7878 (asdfsdf)'
-b = 7
-# c = a and b
-
-ll = a.split()[1]
-# print(ll[0])
-# lll = ll.replace('(','').replace(')','')
-print ll
-# name = ''
-if ll[-1] == ')':
-    ll = ll[:-1]
-    print ll
-if ll[0] == '(':
-    ll = ll[1:]
-    print ll
-
-
-# print a.split()
-# if not a:
-#     print 'c'
+print(aa)
