117c5263ed312936d6b08286dee5db0361deae37
唐钰豆豆 <doudoudzj@gmail.com>
2019-01-22 17:45:34 +0800

    certs update

--- test/ssl/acme.py
+++ test/ssl/acme.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 # Python 2.6 need install python-argparse
 # Copyright Daniel Roesler, under MIT license, see LICENSE at https://github.com/diafygi/acme-tiny
-import argparse
-import subprocess
-import json
-import os
-import sys
+
 import base64
 import binascii
-import time
-import hashlib
-import re
 import copy
-import textwrap
+import hashlib
+import json
 import logging
+import os
+import re
+import subprocess
+import sys
+import textwrap
+import time
+
 try:
+    import argparse  # Python 2.7+
     from urllib.request import urlopen, Request  # Python 3
 except ImportError:
-    from urllib2 import urlopen, Request  # Python 2.7+
+    from urllib2 import urlopen, Request  # Python 2
 
 # DEPRECATED! USE DEFAULT_DIRECTORY_URL INSTEAD
 DEFAULT_CA = "https://acme-v02.api.letsencrypt.org"
@@ -28,7 +30,7 @@ LOGGER.addHandler(logging.StreamHandler())
 LOGGER.setLevel(logging.INFO)
 
 
-class ACME():
+class Certs():
 
     def __init__(self):
         self.path_current = os.path.dirname(os.path.realpath(__file__))
@@ -37,58 +39,79 @@ class ACME():
         self.key_size = '4096'
         self.account_key = self.path_current + '/' + 'account.key'
 
-        self.acme_account_key()
+        if not os.path.exists(self.path_crts):
+            os.makedirs(self.path_crts)
 
-    def _check(self, file_key):
-        if file_key and os.path.exists(file_key):
-            try:
-                exists = self._cmd(
-                    ['openssl', 'rsa', '-in', file_key, '-check'])
-                if 'RSA key ok' in exists and forced == False:
-                    return True
-            except:
-                pass
+        self.init_acme_account()
 
-    # helper function - run external commands
     def _cmd(self, cmd_list, stdin=None, cmd_input=None, err_msg="Command Line Error"):
         proc = subprocess.Popen(cmd_list, stdin=stdin,
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = proc.communicate(cmd_input)
+        out = out if out else err
         if proc.returncode != 0:
             raise IOError("{0}\n{1}".format(err_msg, err))
-        return out
+        return out.decode('utf8')
+
+    def _check_key(self, key):
+        '''check the RSA key is ok'''
+        if not os.path.exists(key):
+            return {'code': -1, 'msg': 'key_not_found'}
+        try:
+            out = self._cmd(['openssl', 'rsa', '-in', key, '-check', '-noout'])
+            if 'RSA key ok' in out:
+                return {'code': 0, 'msg': 'key_ok', 'data': 'ok'}
+            else:
+                return {'code': 0, 'msg': 'key_broken'}
+        except:
+            return {'code': -1, 'msg': 'key_check_error'}
+
+    def _check_csr(self, csr):
+        '''verify the CSR is ok'''
+        csr = csr if os.path.exists(csr) or os.path.isfile(
+            csr) else self.path_crts + csr
+        if not os.path.exists(csr):
+            return {'code': -1, 'msg': 'csr_not_found'}
+        try:
+            # p = subprocess.Popen(['openssl', 'req', '-in', csr, '-verify', '-noout'],
+            #         stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+            # out = p.stdout.read()
+            # msg = p.stderr.read()
+            # out = out if out else msg
+            # if p.wait() == 0:
+            #     if 'verify OK' in out:
+            #         return True
+            #     else:
+            #         return False
+            # else:
+            #     return None
+            out = self._cmd(
+                ['openssl', 'req', '-in', csr, '-verify', '-noout'])
+            if 'verify OK' in out:
+                return {'code': 0, 'msg': 'verify_success'}
+            else:
+                return {'code': -1, 'msg': 'verify_error'}
+        except:
+            return {'code': -1, 'msg': 'verify_error'}
 
     def _generate_private_key(self, file_key=None, forced=False):
-        print(forced, 111)
         '''Generate private key'''
         if file_key == None or file_key == '':
-            print(222)
-            return False
-        if file_key and os.path.exists(file_key):
-            print(333)
-            try:
-                exists = self._cmd(
-                    ['openssl', 'rsa', '-in', file_key, '-check'])
-                if 'RSA key ok' in exists and forced == False:
-                    print(444)
-                    return True
-            except:
-                pass
+            return {'code': -1, 'msg': 'need_key_name'}
+        ckk = self._check_key(file_key)
+        if ckk['code'] == 0 and ckk['data'] == 'ok' and forced == False:
+            return {'code': -1, 'msg': 'key_exists'}
         try:
-            print(555, file_key)
-            cmd_l = ['openssl', 'genrsa', '-out', file_key, self.key_size]
-            print(545, cmd_l)
-            res = self._cmd(cmd_l, err_msg='Generate private key error')
-            print(578, res)
-            if res is None:
-                print(666, res)
-                return False
+            cmd_ = ['openssl', 'genrsa', '-out', file_key, self.key_size]
+            out = self._cmd(cmd_, err_msg='Generate private key error')
+            if out is None:
+                return {'code': -1, 'msg': 'key_generate_error'}
             else:
-                return True
+                return {'code': 0, 'msg': 'key_generate_success'}
         except:
-            return False
+            return {'code': -1, 'msg': 'key_generate_error'}
 
-    def acme_account_key(self, file_key=None, forced=False):
+    def init_acme_account(self, file_key=None, forced=False):
         '''Create a Let's Encrypt account private key'''
         file_key = self.account_key if file_key == None or file_key == '' else file_key
         return self._generate_private_key(file_key=file_key, forced=forced)
@@ -98,67 +121,89 @@ class ACME():
         key = self.path_crts + domain + '.key'
         return self._generate_private_key(file_key=key, forced=forced)
 
-    def create_domain_csr(self, domain=[], domain_key=None):
-        '''Create a certificate signing request (CSR) for domains.'''
+    def generate_domain_csr(self, domain=[], domain_key=None, forced=False):
+        '''Generate a certificate signing request (CSR) for domains.'''
         # openssl genrsa 4096 > github.com.key
-        if len(domain) == 0:
-            return None
-        k = domain_key if domain_key and os.path.exists(
-            domain_key) else self.path_crts + domain[0] + '.key'
-        c = self.path_crts + domain[0] + '.csr'
+        if domain is None or len(domain) == 0:
+            return {'code': -1, 'msg': 'domain_error'}
+        g_domain = domain[0]
+        if domain_key and os.path.isfile(domain_key):
+            k = domain_key
+        else:
+            k = self.path_crts + g_domain + '.key'
+        if not os.path.isfile(k):
+            return {'code': -1, 'msg': 'key_not_found'}
+        c = self.path_crts + g_domain + '.csr'
+        if os.path.isfile(c) and forced == False:
+            return {'code': -1, 'msg': 'csr_exists'}
         if len(domain) == 1:
             cmd_list = ['openssl', 'req', '-new', '-sha256',
-                        '-key', k, '-subj', '/CN=' + domain[0]]
-            tt = self._cmd(
+                        '-key', k, '-subj', '/CN=' + g_domain]
+            out = self._cmd(
                 cmd_list, err_msg="Create certificate signing request Error")
-            if tt is None:
-                return False
+            if out is None:
+                return {'code': -1, 'msg': 'csr_create_error'}
             else:
                 with open(c, 'w') as f:
-                    f.write(tt)
-                return True
+                    f.write(out)
+                return {'code': 0, 'msg': 'csr_create_success', 'data': out}
         if len(domain) > 1:
             sans = []
             for i in domain:
                 sans.append('DNS:%s' % i)
             sans = ','.join(sans)
-            domain_sans = 'Dept/CN=%s/subjectAltName=%s' % (domain[0], sans)
-            cmd_list = ['openssl', 'req', '-new', '-sha256',
-                        '-key', k, '-subj', '-out', c, domain_sans]
-            print(cmd_list)
-            tt = self._cmd(
+            # aa = 'openssl req -new -out effect.pub.csr -key effect.pub.key -config openssl.cnf'
+            domain_sans = '/CN=%s/subjectAltName=%s' % (g_domain, sans)
+            cmd_list = ['openssl', 'req', '-new',
+                        '-sha256', '-key', k, '-subj', domain_sans]
+            out = self._cmd(
                 cmd_list, err_msg="Create certificate signing request Error")
-            if tt is None:
-                return False
+            if out is None:
+                return {'code': -1, 'msg': 'csr_create_error'}
             else:
-                # with open(c, 'w') as f:
-                #     f.write(tt)
-                return True
+                with open(c, 'w') as f:
+                    f.write(out)
+                return {'code': 0, 'msg': 'csr_create_success', 'data': out}
 
-    def singature(self):
-        path_current = self.path_current
-        signed_crt = get_acme_crt(path_current + '/account.key', path_current +
-                                  '/dougroup.com.csr', '/var/www/dougroup.com/.well-known/acme-challenge/')
-        if signed_crt is not None:
-            with open(path_current + '/dougroup.com.crt', 'w') as f:
-                f.write(signed_crt)
-
-    def show_domain_csr(self, domain_key, text=False, pubkey=False, subject=False):
+    def show_domain_csr(self, domain_csr=None, text=True, pubkey=False, subject=False):
+        # domain_csr = self.path_crts + domain_csr
+        if not domain_csr:
+            return {'code': -1, 'msg': 'csr_not_found'}
+        csr = domain_csr if os.path.exists(domain_csr) and os.path.isfile(
+            domain_csr) else self.path_crts + domain_csr
+        ckcsr = self._check_csr(csr)
+        if not ckcsr['code'] == 0:
+            return ckcsr
         try:
-            cmd_list = ['openssl', 'req', '-in', domain_key]
-            if text is True:
-                cmd_list.append('-text')
+            cmd_ = ['openssl', 'req', '-in', csr]
+            if text == True and pubkey is not True and subject is not True:
+                cmd_.append('-text')
             if pubkey is True:
-                cmd_list.append('-noout -pubkey')
+                cmd_.extend(['-noout', '-pubkey'])
             if subject is True:
-                cmd_list.append('-noout -subject')
-            res = self._cmd(cmd_list, err_msg='Generate private key error')
-            if res is None:
-                return False
+                cmd_.extend(['-noout', '-subject'])
+            out = self._cmd(cmd_, err_msg='Generate private key error')
+            if out is None:
+                return {'code': -1, 'msg': 'csr_read_error'}
             else:
-                return res
+                return {'code': 0, 'msg': 'csr_read_success', 'data': out}
         except:
+            return {'code': -1, 'msg': 'csr_read_error'}
+
+    def singature(self, host=''):
+        if not host:
             return None
+        acc = self.account_key
+        csr = '%s%s.csr' % (self.path_crts, host)
+        crt = '%s%s.crt' % (self.path_crts, host)
+        ckdir = '/var/www/%s/.well-known/acme-challenge' % host
+        print(acc, csr, crt, ckdir)
+        if not os.path.exists(ckdir):
+            os.makedirs(ckdir)
+        signed_crt = get_acme_crt(acc, csr, ckdir)
+        if signed_crt is not None:
+            with open(crt, 'w') as f:
+                f.write(signed_crt)
 
 
 def get_acme_crt(account_key, csr, acme_dir, log=LOGGER, CA=DEFAULT_CA, disable_check=False, directory_url=DEFAULT_DIRECTORY_URL, contact=None):
@@ -386,10 +431,18 @@ def main(argv=None):
 
 
 if __name__ == "__main__":  # pragma: no cover
-    acme = ACME()
-    # acme.acme_account_key()
+    acme = Certs()
+    # acme.init_acme_account()
+    # print(acme.init_acme_account())
     # main(sys.argv[1:])
     # singature()
-    # acme = ACME()
-    # print(acme.create_domain_key('effect.pub'))
-    print(acme.create_domain_csr(['effect.pub', 'abc.effect.pub']))
+    # print(acme.create_domain_key('baokan.pub',forced=True))
+    # print(acme.generate_domain_csr(['baokan.pub'],forced=True))
+    print(acme.generate_domain_csr(['baokan.pub', 'www.baokan.pub', 'abc.baokan.pub'],forced=True))
+    # print(acme.generate_domain_csr(['effect.pub', '*.effect.pub']))
+    # print(acme._check_csr('effect.pub.csr'))
+    # acme._check_csr('effect.pub.csr')
+
+    print(acme.show_domain_csr('baokan.pub.csr', subject=True))
+    # acme.show_domain_csr('effect.pub.csr')
+    # acme.singature('baokan.pub')
