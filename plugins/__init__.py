c3d93b64b51a601ca72511aa8943992888d019f0
唐钰豆豆 <doudoudzj@gmail.com>
2019-02-22 16:44:01 +0800

    plugins

---
+++ plugins/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 - 2019, doudoudzj
+# All rights reserved.
+#
+# InPanel is distributed under the terms of the (new) BSD License.
+# The full license can be found in 'LICENSE'.
+
+'''Plugins for InPanel'''
+
