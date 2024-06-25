27e2045105dd115a83f19578ec65b6eaf332b593
Jackson Dou <jksdou@qq.com>
2024-06-25 17:01:03 +0800

    update: InPanel v1.1.1 b30

--- core/__init__.py
+++ core/__init__.py
@@ -14,7 +14,7 @@ import platform
 name = 'InPanel'
 version = '1.1.1'
 build = '30'
-releasetime = '2024-03-06 16:51:00 GMT+0800'
+releasetime = '2024-06-25'
 __version__ = '.'.join([version, build])
 
 version_info = {
